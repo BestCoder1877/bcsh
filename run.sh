f2e29fcd9a60671148567b03e75ec44ca8c8ee74
bestCoder1877 <datoohamza@outlook.com>
2026-08-30 09:18:33 -0400

    Switched To shell.nix

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 set -e
 
+nix-shell shell.nix --run '
 cmake -S . -B build-x86_64 \
-    -DCMAKE_C_COMPILER=gcc \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_FLAGS="-O2 -static"
+  -DCMAKE_C_COMPILER=gcc \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_C_FLAGS="-O2 -static"
 cmake --build build-x86_64
 chmod +x ./build-x86_64/bcsh
 ./build-x86_64/bcsh
 rm -rf ./build-x86_64
+'
+
