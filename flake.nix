aaacdfc7484519a9f7b865cab32113f8b361dbab
bestCoder1877 <datoohamza@outlook.com>
2026-09-01 13:52:37 -0400

    Fixed Flake For Rust

--- flake.nix
+++ flake.nix
@@ -1,24 +1,27 @@
 {
   description = "The Best Coder Shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
   outputs =
-    { nixpkgs, ... }:
-    let
-      system = builtins.currentSystem;
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-        pname = "bcsh";
-        version = "unstable";
-        src = ./.;
-        cargoLock.lockFile = ./Cargo.lock;
-        buildType = "release";
-        RUSTFLAGS = "-C target-feature=+crt-static";
-        installPhase = ''
-          mkdir -p $out/bin
-          cp target/release/bcsh $out/bin/bcsh
-        '';
-      };
-    };
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "bcsh";
+          version = "unstable";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          buildType = "release";
+          RUSTFLAGS = "-C target-feature=+crt-static";
+          installPhase = ''
+            mkdir -p $out/bin
+            cp target/release/bcsh $out/bin/bcsh
+          '';
+        };
+      });
 }
